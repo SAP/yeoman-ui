0b02d5017ba4e37b9b92a9359c89ce29bbdc1ba6
i040767 <ido.perez@sap.com>
2019-10-13 15:08:29 +0300

    add selected answer to confirm type

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -177,7 +177,7 @@ export default {
 
       let prompt1 = {
         name: "Step 1",
-        questions: [checkboxQ]
+        questions: [confirmQ]
       };
       let prompt2 = {
         name: "Step 2",
