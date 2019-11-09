09661b3df0ea285120601044510caee4b5796600
Ido Perez <ido.perez@sap.com>
2019-11-09 06:44:13 +0200

    hide questions if when return false

--- vue/src/components/QuestionTypeSelector.vue
+++ vue/src/components/QuestionTypeSelector.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-form-group :label="currentQuestion.message" :disabled="!currentQuestion.isWhen">
+    <b-form-group :label="currentQuestion.message" v-if="currentQuestion.isWhen">
       <GeneratorSelection v-if="currentQuestion.type==='generators'" :currentQuestion="currentQuestion" v-on:generatorSelected="onGeneratorSelected"/>
 
       <QuestionInput v-if="currentQuestion.type==='input' || currentQuestion.type==='password' || currentQuestion.type==='number'" :currentQuestion="currentQuestion" />
