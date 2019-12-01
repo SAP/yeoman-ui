ff6b2201e33831bf5a5e5e79e1eb2925900e0a71
Stanislav Lvovsky <stanislav.lvovsky@sap.com>
2019-12-01 07:03:53 +0200

    jest config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,30 +1,31 @@
 module.exports = {
-    collectCoverage: true,
-    collectCoverageFrom: [
-      "**/src/**/*.{js,vue}",
-      "!**/node_modules/**"
-    ],
-    coverageReporters: [
-      "html",
-      "text-summary"
-    ],
-    moduleFileExtensions: [
-      "js",
-      "vue",
-      "json"
-    ],
-    modulePaths: [
-      "<rootDir>/src",
-      "<rootDir>/node_modules"
-    ],
-    transform: {
-      ".*\\.(vue)$": "vue-jest",
-      '^.+\\.vue$': 'vue-jest',
-      '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-      '^.+\\.tsx?$': 'ts-jest',
-      "^.+\\.js$": "<rootDir>/node_modules/babel-jest"
-    },
-    snapshotSerializers: [
-      "<rootDir>/node_modules/jest-serializer-vue"
-    ]
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,vue}",
+    "!**/node_modules/**"
+  ],
+  coverageReporters: [
+    "html",
+    "text-summary"
+  ],
+  moduleFileExtensions: [
+    "js",
+    "vue",
+    "json"
+  ],
+  modulePaths: [
+    "<rootDir>/src",
+    "<rootDir>/node_modules"
+  ],
+  transform: {
+    ".*\\.(vue)$": "vue-jest",
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest',
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest"
+  },
+  snapshotSerializers: [
+    "<rootDir>/node_modules/jest-serializer-vue"
+  ]
 }
