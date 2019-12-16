4c7ceffbb370f94092785ff9845da575dd40af63
Stanislav Lvovsky <stanislav.lvovsky@sap.com>
2019-12-16 13:18:40 +0200

    add filter test

--- backend/src/filter.ts
+++ backend/src/filter.ts
@@ -5,16 +5,35 @@ export enum GeneratorType {
     module = "module"
 }
 
-export class GeneratorFilter {
-    private constructor(public type?: GeneratorType,
-    public cetegories: string[] = []) {}
-
-    public static create(filterObject: any) {
-        const genType: GeneratorType = _.get(filterObject, "type");
-        if (_.includes(_.values(GeneratorType), genType)) {
-            return new GeneratorFilter(genType);
+function getCategories(filterObject?: any): string[] {
+    const categories: string[] = _.get(filterObject, 'categories', []);
+    if (_.isArray(categories)) {
+        const strValues = _.filter(categories, category => {
+            return _.isString(category);
+        });
+        if (_.isEmpty(_.difference(categories, strValues))) {
+            return categories;
         }
+    }
 
-        return new GeneratorFilter();
+    return [];
+}
+
+function getType(filterObject?: any): GeneratorType {
+    const genType: GeneratorType = _.get(filterObject, "type");
+    if (_.includes(_.values(GeneratorType), genType)) {
+        return GeneratorType[genType];
+    }
+}
+
+export class GeneratorFilter {
+    constructor(public readonly  type?: GeneratorType,
+                public readonly categories: string[] = []) {}
+
+    public static create(filterObject?: any) {
+        const categories: string[] = getCategories(filterObject); 
+        const type: GeneratorType = getType(filterObject);
+
+        return new GeneratorFilter(type, categories);
     }
 }
---
+++ backend/tests/filter.spec.ts
@@ -0,0 +1,38 @@
+import * as mocha from "mocha";
+import { expect } from "chai";
+
+import {GeneratorFilter, GeneratorType} from "../src/filter";
+
+describe('filter unit test', () => {
+    it('categories property is not of array type', () => {
+        const genFilter: GeneratorFilter = GeneratorFilter.create({categories: {}});
+        expect(genFilter.categories).to.be.deep.equal([]);
+    });
+
+    it('categories property array hold values other than strings', () => {
+        const genFilter: GeneratorFilter = GeneratorFilter.create({categories: ["test", true, {}]});
+        expect(genFilter.categories).to.be.deep.equal([]);
+    });
+
+    it('type property is not equal to any declared type property', () => {
+        const testCategories: string[] = ["test1", "test2"];
+        const genFilter: GeneratorFilter = GeneratorFilter.create({type: "test123", categories: testCategories});
+        // tslint:disable-next-line: no-unused-expression
+        expect(genFilter.type).to.be.undefined;
+        expect(genFilter.categories).to.be.deep.equal(testCategories);
+    });
+
+    it('type property is project and category property has strings in array ', () => {
+        const testCategories: string[] = ["test1", "test2"];
+        const genFilter: GeneratorFilter = GeneratorFilter.create({type: "project", categories: testCategories});
+        expect(genFilter.type).to.be.equal(GeneratorType.project);
+        expect(genFilter.categories).to.be.deep.equal(testCategories);
+    });
+
+    it('type property is module and category property has strings in array ', () => {
+        const testCategories: string[] = ["test1", "test2"];
+        const genFilter: GeneratorFilter = GeneratorFilter.create({type: "module", categories: testCategories});
+        expect(genFilter.type).to.be.equal(GeneratorType.module);
+        expect(genFilter.categories).to.be.deep.equal(testCategories);
+    });
+});
