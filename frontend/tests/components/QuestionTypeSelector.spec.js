316d28d075a9042308e9e89773f623519dcd5839
i348307 <doron.nahari@sap.com>
2020-01-12 18:12:41 +0200

    test

--- frontend/tests/components/QuestionTypeSelector.spec.js
+++ frontend/tests/components/QuestionTypeSelector.spec.js
@@ -17,28 +17,4 @@ describe('QuestionTypeSelector.vue', () => {
         expect(wrapper.vm.isValid({isValid: false, validationMessage: "test"} )).toBe("test")
 
     })
-
-    // test('component props', () => {
-    //     wrapper = initComponent(Header)
-    //     expect(_.keys(wrapper.props())).toHaveLength(3)
-    // })
-
-    // test('generator brand', () => {
-    //     const testGen = 'Selected Generator: testGenerator'
-    //     wrapper = initComponent(Header, { selectedGeneratorHeader: testGen })
-    //     expect(wrapper.find('v-toolbar-title-stub').text()).toBe(testGen)
-    //     expect(wrapper.find('v-icon-stub').text()).toBe("mdi-console")
-    // })
-
-    // test('click triggers collapseLog method', async () => {
-    //     const rpcInvokeMockFunction = jest.fn()
-    //     wrapper = initComponent(Header, {
-    //         rpc: {
-    //             invoke: rpcInvokeMockFunction
-    //         }
-    //     }, true)
-        
-    //     wrapper.find("button").trigger('click')
-    //     expect(rpcInvokeMockFunction).toHaveBeenCalled()  
-    // })
 })
