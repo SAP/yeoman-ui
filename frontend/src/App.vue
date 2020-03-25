fc223f1bb7046b964450a13abeb8a13f19748305
Tomer Epstein <57438361+tomer-epstein@users.noreply.github.com>
2020-03-25 12:37:35 +0200

    set-back-invisable (#202)
    * set-back-invisable
    
    * "version": "0.0.64"

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -58,7 +58,7 @@
           <div class="diagonal">
           </div>
           <div class="bottom-buttons-col" style="display:flex;align-items: center;">
-            <v-btn id="back" :disabled="promptIndex<1 || isReplaying" @click="back" v-show="!shouldShowGeneratorSelection()">
+            <v-btn id="back" :disabled="promptIndex<1 || isReplaying" @click="back" v-show="false && !shouldShowGeneratorSelection()">
               <v-icon left>mdi-chevron-left</v-icon>Back
             </v-btn>
             <v-btn id="next" :disabled="!stepValidated" @click="next">
