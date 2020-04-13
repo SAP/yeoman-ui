13ca3958c2e36fd42f03d1f74b938637eec0e73d
Tomer Epstein <tomer.epstein@sap.com>
2020-04-13 18:06:48 +0300

    inquirer-gui-0.1.6

--- generator-foodq/generators/app2/index.js
+++ generator-foodq/generators/app2/index.js
@@ -1,5 +1,8 @@
 var Generator = require('yeoman-generator');
 var _ = require('lodash');
+var path = require('path');
+const datauri = require("datauri");
+const DEFAULT_IMAGE = require("./images/defaultImage");
 
 module.exports = class extends Generator {
   constructor(args, opts) {
@@ -31,8 +34,8 @@ module.exports = class extends Generator {
         name: "deliveryMethod",
         message: "Delivery method",
         choices: [
-          "Car",
-          "Drone"
+          { value: "car", name: "Car", image: this._getImage(path.join(this.sourceRoot(), "../images/car.png")) },
+          { value: "drone", name: "Drone", image: this._getImage(path.join(this.sourceRoot(), "../images/drone.png")) }
         ],
         when: answers => {
           const indexOfAddress = _.findIndex(this.prompts.items, prompt => {
@@ -82,4 +85,16 @@ module.exports = class extends Generator {
 
     this.answers = await this.prompt(tipPrompt);
   }
+
+  _getImage(imagePath) {
+    let image;
+    try {
+      image = datauri.sync(imagePath);
+    } catch (error) {
+      image = DEFAULT_IMAGE;
+      this.log("Error", error);
+    }
+    return image;
+  }
+
 };
