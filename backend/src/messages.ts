061503ad2be0383b527fbf1c39cdb578a1f45f5b
Slavik Lvovsky <stanislav.lvovsky@sap.com>
2020-04-26 10:31:40 +0300

    update dependencies and messages

--- backend/src/messages.ts
+++ backend/src/messages.ts
@@ -4,7 +4,7 @@ export default {
     step_is_pending: "Loading...",
     yeoman_ui_title: "Yeoman Generators",
     select_generator_name: "Select Generator",
-    select_generator_question_message: "Select Generator",
+    select_generator_question_message: "",
     channel_name: "Generators",
     select_generator_description: "Select the generator that best fits the type of application you want to develop.\nThe project will contain the relevant files and configurations required to create your application."
 }; 
