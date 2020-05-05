16c4aad1d8dcdcc3eae7b7cdf81082002809fc01
Tomer Epstein <tomer.epstein@sap.com>
2020-05-05 15:34:45 +0300

    add support for hints

--- backend/src/messages.ts
+++ backend/src/messages.ts
@@ -1,5 +1,5 @@
-const select_generator_question_hint = "Select the generator that best fits the type of application you want to develop";
-const select_target_folder_question_hint = "The project will contain the relevant files and configurations required to create your application";
+const selectGeneratorQuestionHint = "Select the generator that best fits the type of application you want to develop";
+const selectTargetFolderQuestionHint = "The project will contain the relevant files and configurations required to create your application";
 
 export default {
     panel_title: "Yeoman UI",
@@ -8,8 +8,8 @@ export default {
     yeoman_ui_title: "Yeoman Generators",
     select_generator_name: "Select Generator",
     select_generator_question_message: "Generators",
-    select_generator_question_hint,
-    select_target_folder_question_hint,
+    select_generator_question_hint: selectGeneratorQuestionHint,
+    select_target_folder_question_hint: selectTargetFolderQuestionHint,
     channel_name: "Generators",
-    select_generator_description: `${select_generator_question_hint}.\n${select_target_folder_question_hint}.`
+    select_generator_description: `${selectGeneratorQuestionHint}.\n${selectTargetFolderQuestionHint}.`
 }; 
