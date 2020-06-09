d80c2bc99aa784b18c20570a55bfeb4a4727aa85
Stanislav Lvovsky <stanislav.lvovsky@sap.com>
2020-06-10 01:37:24 +0300

    handle uncaught exception

--- backend/src/yeomanui.ts
+++ backend/src/yeomanui.ts
@@ -186,33 +186,30 @@ export class YeomanUI {
       this.gen.destinationRoot(targetFolder);
       
       env.on("error", error => {
-        this.showErrorOnFailure(env, generatorName, error);
+        env.removeAllListeners("error");
+        this.onGeneratorFailure(generatorName, error);
+        env.emit("error", error);
       });
 
       this.gen.on("error", error => {
-        this.showErrorOnFailure(this.gen, generatorName, error);
+        this.onGeneratorFailure(generatorName, error);
       });
+
+      process.on('uncaughtException', error => {
+        this.onGeneratorFailure(generatorName, error);
+      });
+
       // we cannot use new async method, "await this.gen.run()", because generators based on older versions 
       // (for example: 2.0.5) of "yeoman-generator" do not support it
       this.gen.run(async (error) => {
-        if (!this.errorThrown) {
-          if (error) {
-            this.onGeneratorFailure(generatorName, error);
-          } else {
+        if (!this.errorThrown && !error) {
             const dirsAfter = await this.getChildDirectories(this.gen.destinationRoot());
             this.onGeneratorSuccess(generatorName, dirsBefore, dirsAfter);
-          }
         }
       });
     } catch (error) {
       this.onGeneratorFailure(generatorName, error);
     }
-  }
-
-  private showErrorOnFailure(obj: any, generatorName: string, error: any) {
-    obj.removeAllListeners("error");
-    this.onGeneratorFailure(generatorName, error);
-    obj.emit("error", error);
   }
 
   /**
