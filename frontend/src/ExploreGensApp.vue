8ce376da82f7f5009d841149aa878fd3878404eb
Stanislav Lvovsky <stanislav.lvovsky@sap.com>
2020-06-29 07:47:52 +0300

    update ui

--- frontend/src/ExploreGensApp.vue
+++ frontend/src/ExploreGensApp.vue
@@ -1,90 +1,91 @@
 <template>
   <v-app id="exploregens">
-    <v-container class="explore-generators">
-      <div id="prompt-info-component">
-        <div>
-          <v-card-title class="explore-generators-title">Explore Generators</v-card-title>
-          <v-expansion-panels class="explore-generators-description" flat>
-            <v-expansion-panel>
-              <v-expansion-panel-header disable-icon-rotate>
-                This view enables the exploration and installation of external open source Yeoman generators.
-                <template
-                  v-slot:actions
-                >
-                  <v-icon color="primary">$expand</v-icon>
-                </template>
-              </v-expansion-panel-header>
-              <v-expansion-panel-content>
-                <v-row>
-                  <v-col :cols="1">
-                    <v-icon color="blue">mdi-information-outline</v-icon>
-                  </v-col>
-                  <v-col :cols="11">
-                    <v-text>NOTE: IN NO EVENT WILL SAP BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, PUNITIVE, SPECIAL ORCONSEQUENTIAL DAMAGES RELATED TO ANY USE OF EXTERNAL GENERATORS EXPLORED AND INSTALLED.</v-text>
-                  </v-col>
-                </v-row>
-              </v-expansion-panel-content>
-            </v-expansion-panel>
-          </v-expansion-panels>
-        </div>
-        <v-row class="prompts-col">
-          <v-col :cols="10">
-            <v-text-field label="Search for Generators" v-model="query" @input="onQueryChange" />
-          </v-col>
-          <v-col :cols="2">
-            <v-select
-              :items="items"
-              v-model="recommended"
-              label="Recommended"
-              @change="onQueryChange"
-            />
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col :cols="3">
-            <v-text-label>{{searchResults}}</v-text-label>
-          </v-col>
-          <v-row v-if="refineSearch">
-            <v-icon color="blue">mdi-information-outline</v-icon>
-            <v-text-label>You may refine your search</v-text-label>
-          </v-row>
-          <!-- <v-col :cols="9" v-if="refineSearch">
-            <v-text-label>You may refine your search</v-text-label>
-          </v-col> -->
-        </v-row>
-        <v-row>
-          <v-col md="3" class="pa-3 d-flex flex-column" v-for="(gen, i) in gens" :key="i">
-            <v-card width="300" class="d-flex flex-column mx-auto" height="300" tile elevation="2">
-              <v-card-title primary-title>
-                <h3 class="headline mb-0">{{ gen.package.name }}</h3>
-              </v-card-title>
-              <v-card-text style="overflow-y: auto; height:200px" v-text="gen.package.description" />
-              <v-card-subtitle v-text="gen.package.version" />
-              <v-card-text class="homepage">
-                <a :href="gen.package.links.npm">More information\</a>
-              </v-card-text>
-              <v-card-actions>
-                <v-menu bottom left>
-                  <template v-slot:activator="{on}">
-                    <v-btn icon v-on="on" :loading="gen.disabledToHandle" @click="getActions(gen)">
-                      <v-icon>mdi-cog-outline</v-icon>
-                    </v-btn>
-                  </template>
-                  <v-list v-if="gen.actions.length > 0">
-                    <v-list-item
-                      v-for="(item, i) in gen.actions"
-                      :key="i"
-                      @click="onAction(gen, item)"
-                    >
-                      <v-list-item-title>{{ item }}</v-list-item-title>
-                    </v-list-item>
-                  </v-list>
-                </v-menu>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
+    <v-container id="explore" class="explore-generators">
+      <div>
+        <v-card-title class="explore-generators-title">Explore Generators</v-card-title>
+        <v-expansion-panels class="explore-generators-description" flat>
+          <v-expansion-panel>
+            <v-expansion-panel-header disable-icon-rotate>
+              This view enables the exploration and installation of external open source Yeoman generators.
+              <template
+                v-slot:actions
+              >
+                <v-icon color="primary">$expand</v-icon>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <v-row>
+                <v-col :cols="1">
+                  <v-icon color="blue">mdi-information-outline</v-icon>
+                </v-col>
+                <v-col :cols="11">
+                  <v-text>NOTE: IN NO EVENT WILL SAP BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, PUNITIVE, SPECIAL ORCONSEQUENTIAL DAMAGES RELATED TO ANY USE OF EXTERNAL GENERATORS EXPLORED AND INSTALLED.</v-text>
+                </v-col>
+              </v-row>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
       </div>
+      <v-row class="prompts-col">
+        <v-col :cols="10">
+          <v-text-field label="Search for Generators" v-model="query" @input="onQueryChange" />
+        </v-col>
+        <v-col :cols="2">
+          <v-select
+            :items="items"
+            v-model="recommended"
+            label="Recommended"
+            @change="onQueryChange"
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col :cols="3">
+          <v-text-label>{{searchResults}}</v-text-label>
+        </v-col>
+        <v-row v-if="refineSearch">
+          <v-icon color="blue">mdi-information-outline</v-icon>
+          <v-text-label>You may refine your search</v-text-label>
+        </v-row>
+      </v-row>
+      <v-row>
+        <v-col md="3" class="pa-3 d-flex flex-column" v-for="(gen, i) in gens" :key="i">
+          <v-card width="300" class="d-flex flex-column mx-auto" height="300" tile elevation="2">
+            <v-card-title primary-title>
+              <h3 class="headline mb-0">{{ gen.package.name }}</h3>
+            </v-card-title>
+            <v-card-text style="overflow-y: auto; height:200px" v-text="gen.package.description" />
+            <v-card-subtitle v-text="gen.package.version" />
+            <v-card-text class="homepage">
+              <a :href="gen.package.links.npm">More information</a>
+            </v-card-text>
+            <v-card-actions>
+              <v-menu bottom left>
+                <template v-slot:activator="{on}">
+                  <v-btn
+                    class="explore-generators-loading"
+                    icon
+                    v-on="on"
+                    :loading="gen.disabledToHandle"
+                    @click="getActions(gen)"
+                  >
+                    <v-icon>mdi-cog-outline</v-icon>
+                  </v-btn>
+                </template>
+                <v-list v-if="gen.actions.length > 0">
+                  <v-list-item
+                    v-for="(item, i) in gen.actions"
+                    :key="i"
+                    @click="onAction(gen, item)"
+                  >
+                    <v-list-item-title>{{ item }}</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
   </v-app>
 </template>
@@ -196,6 +197,10 @@ export default {
 };
 </script>
 <style scoped>
+.explore-generators-loading.theme--light.v-btn.v-btn--icon {
+  color: var(--vscode-progressBar-background, #0e70c0);
+}
+.explore-generators .theme--light.v-expansion-panels .v-expansion-panel,
 .explore-generators .v-card {
   background-color: var(--vscode-editorWidget-background, #252526);
 }
@@ -206,6 +211,7 @@ export default {
   border: 1px solid var(--vscode-button-background, #0e639c);
   background-color: var(--vscode-list-hoverBackground, #2a2d2e);
 }
+.explore-generators .theme--light.v-expansion-panels .v-expansion-panel,
 .explore-generators .theme--light.v-card .v-card__subtitle.v-card__subtitle,
 .explore-generators .v-icon.v-icon,
 .explore-generators .v-card__title {
--- frontend/src/exploreGensMain.js
+++ frontend/src/exploreGensMain.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import ExploreGensApp from './ExploreGensApp'
 import vuetify from './plugins/vuetify';
+import "./assets/css/globalStyles.css";
 
 Vue.config.productionTip = false
 
