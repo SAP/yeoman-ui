0c42739e3d3f90fcf35568fab0218bbcfabb4abf
Stanislav Lvovsky <stanislav.lvovsky@sap.com>
2020-07-06 15:08:31 +0300

    create run configuration

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,14 @@
+const _ = require("lodash");
+const path = require("path");
+
+
+const pPath = _.get(process, "argv[3]") ? path.join(".", "exploregens") : path.join(".");
+// eslint-disable-next-line no-console
+console.error("HELLO - " + pPath);
+
 module.exports = {
   runtimeCompiler: true,
-  publicPath: "./",
+  publicPath: pPath,
   transpileDependencies: ["vuetify"],
   productionSourceMap: false,
   configureWebpack: {
@@ -9,9 +17,9 @@ module.exports = {
   pages: {
     index: {
       // entry for the page
-      entry: 'src/main.js',
+      entry: path.join("src", "main.js"),
       // the source template
-      template: 'public/index.html',
+      template: path.join("public", "index.html"),
       // output as dist/index.html
       filename: 'index.html',
       // chunks to include on this page, by default includes
@@ -20,11 +28,11 @@ module.exports = {
     },
     exploreGensIndex: {
       // entry for the page
-      entry: 'src/exploreGensMain.js',
+      entry: path.join("src", "exploreGensMain.js"),
       // the source template
-      template: 'public/exploregens/index.html',
+      template: path.join("public", "exploregens", "index.html"),
       // output as dist/exploregens/index.html
-      filename: 'exploregens/index.html',
+      filename: "index.html", // filename: path.join("exploregens", "index.html"),
       // chunks to include on this page, by default includes
       // extracted common chunks and vendor chunks.
       chunks: ['chunk-vendors', 'chunk-common', 'exploreGensIndex']
