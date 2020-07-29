f070e2eaa09015b56e5b3d38d3627d26fad5729e
${configUser} <neta.molcho@sap.com>
2020-07-29 09:01:36 +0300

    Disables the automatic Caps Lock

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -3,7 +3,11 @@
     <v-app-bar class="elevation-0">
       <v-toolbar-title>{{headerTitle}}</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn v-if="isGeneric" text x-small color="primary" class="ma-2" @click="openExploreGenerators">Explore and Install Generators...</v-btn>
+      <v-btn v-if="isGeneric" text x-small color="primary" @click="openExploreGenerators">
+        <v-card-text>
+          Explore and Install Generators...
+        </v-card-text>
+      </v-btn>
       <v-btn v-if="!isInVsCode" class="ma-2" icon @click="collapseOutput">
         <v-card-text>
           <v-icon>mdi-console</v-icon>
@@ -33,6 +37,7 @@ header.v-app-bar.v-toolbar,
 header.v-app-bar.v-toolbar .v-btn {
   background-color: var(--vscode-editor-background, #1e1e1e);
   color: var(--vscode-foreground, #cccccc);
+  text-transform: none;
 }
 header.v-app-bar.v-toolbar {
   border-bottom: 1px solid var(--vscode-editorWidget-background, #252526);
