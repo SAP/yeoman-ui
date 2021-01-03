45f2a10fe572bc57c749914c828e84705d6e40e7
Stanislav Lvovsky <stanislav.lvovsky@sap.com>
2021-01-03 18:06:51 +0200

    feat: support lcnc

--- generator-foodq/generators/app2/index.js
+++ generator-foodq/generators/app2/index.js
@@ -12,54 +12,71 @@ module.exports = class extends Generator {
 		this.appWizard = opts.appWizard;
 		this.parentPromptsQuantity = _.size(this.prompts);
 
+		this.option("lcnc", { type: Boolean });
+		this.argument("isDelivery", { type: Boolean, required: false, default: (this.options.lcnc ? true : undefined) });
+		this.argument("deliveryMethod", { type: Array, required: false, default: (this.options.lcnc ? "Drone" : undefined) });
+		this.argument("address", { type: String, required: false, default: (this.options.lcnc ? "the best street" : undefined) });
+		this.argument("tip", { type: Number, required: false, default: (this.options.lcnc ? 20 : undefined) });
+
 		this.dynamicAddressPrompt = { name: "Address", description: "Provide the address for delivery." };
 
-		const prompts = [
-			{ name: "Delivery", description: "Select your prefered delivery method." },
-			this.dynamicAddressPrompt,
-			{ name: "Tip", description: "You can include a tip for the delivery person." }];
+		const prompts = [{
+			name: "Delivery", description: "Select your prefered delivery method."
+		},
+		this.dynamicAddressPrompt,
+		{
+			name: "Tip", description: "You can include a tip for the delivery person."
+		}];
 
-		if (this.prompts){
+		if (this.prompts) {
 			this.prompts.splice(this.parentPromptsQuantity, 0, prompts);
 		}
 	}
 
+	_getAnswer(name, res) {
+		return this._getOption(name) || _.get(res, `[${name}]`);
+	}
+
+	_getOption(name) {
+		return _.get(this.options, `[${name}]`);
+	}
+
 	async prompting() {
 		// isDelivery question appears twice in CLI: https://github.com/yeoman/generator/issues/1100
-		const prompts = [
-			{
-				type: "confirm",
-				name: "isDelivery",
-				message: "Do you want the food delivered to your home?",
-				default: true
+		const prompts = [{
+			type: "confirm",
+			name: "isDelivery",
+			message: "Do you want the food delivered to your home?",
+			default: true,
+			when: () => _.isNil(this._getOption("isDelivery"))
+		}, {
+			type: "list",
+			guiOptions: {
+				type: "tiles",
+				hint: "Select the preferred delivery method"
 			},
-			{
-				type: "list",
-				guiOptions: {
-					type: "tiles",
-					hint: "Select the preferred delivery method"
-				},
-				name: "deliveryMethod",
-				message: "Delivery method",
-				choices: [
-					{ value: "car", name: "Car", image: this._getImage(path.join(this.sourceRoot(), "../images/car.png")) },
-					{ value: "drone", name: "Drone", image: this._getImage(path.join(this.sourceRoot(), "../images/drone.png")) }
-				],
-				validate: (value) => {
-					if (value === "car") {
-						this.appWizard.showWarning("Car delivery is not reliable.", types.MessageType.prompt);
-					} else {
-						this.appWizard.showInformation("Drone is very fast.", types.MessageType.prompt);
-					}
-					return true;
-				},
-				when: answers => {
+			name: "deliveryMethod",
+			message: "Delivery method",
+			choices: [
+				{ value: "car", name: "Car", image: this._getImage(path.join(this.sourceRoot(), "../images/car.png")) },
+				{ value: "drone", name: "Drone", image: this._getImage(path.join(this.sourceRoot(), "../images/drone.png")) }
+			],
+			validate: (value) => {
+				if (value === "car") {
+					this.appWizard.showWarning("Car delivery is not reliable.", types.MessageType.prompt);
+				} else {
+					this.appWizard.showInformation("Drone is very fast.", types.MessageType.prompt);
+				}
+				return true;
+			},
+			when: answers => {
+				if (_.isNil(this._getOption("deliveryMethod"))) {
 					const indexOfAddress = _.findIndex(_.get(this.prompts, "items"), prompt => {
 						return prompt.name === this.dynamicAddressPrompt.name;
 					});
 
 					let bWhen = false;
-					if (answers.isDelivery) {
+					if (this._getAnswer("isDelivery", answers)) {
 						// add address prompt if doesn't exist
 						if (indexOfAddress === -1 && this.prompts) {
 							this.prompts.splice(this.parentPromptsQuantity + 1, 0, this.dynamicAddressPrompt);
@@ -74,37 +91,39 @@ module.exports = class extends Generator {
 
 					return bWhen;
 				}
+				return false;
 			}
-		];
+		}];
 
 		this.answers = await this.prompt(prompts);
+		this.answers.isDelivery = this._getAnswer("isDelivery", this.answers);
 
-		if (this.answers.isDelivery) {
-			const addressPrompt = [
-				{
-					type: "input",
-					name: "address",
-					message: "Home address",
-					filter: function (value) {
-						return `(${value})`
-					}
-				}
-			];
+		if (this._getAnswer("isDelivery", this.answers)) {
+			const addressPrompt = [{
+				type: "input",
+				name: "address",
+				message: "Home address",
+				filter: function (value) {
+					return `(${value})`
+				},
+				when: () => _.isNil(this._getOption("address"))
+			}];
 			const answersDelivery = await this.prompt(addressPrompt);
 			this.answers = Object.assign({}, this.answers, answersDelivery);
+			this.answers.address = this._getAnswer("address", this.answers);
 		}
 
-		const tipPrompt = [
-			{
-				type: "number",
-				name: "tip",
-				message: "How much would you like to tip?",
-				default: "10"
-			}
-		];
+		const tipPrompt = [{
+			type: "number",
+			name: "tip",
+			message: "How much would you like to tip?",
+			default: "10",
+			when: () => _.isNil(this._getOption("tip"))
+		}];
 
 		const answersTip = await this.prompt(tipPrompt);
 		this.answers = Object.assign({}, this.answers, answersTip);
+		this.answers.tip = this._getAnswer("tip", this.answers);
 	}
 
 	writing() {
