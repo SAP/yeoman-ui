b8f26d391513712bdb7a5a4323dd1ff37faa11cf
Stanislav Lvovsky <stanislav.lvovsky@sap.com>
2021-05-13 21:49:56 +0300

    chore: format fix

--- packages/backend/src/logger/logger-wrapper.ts
+++ packages/backend/src/logger/logger-wrapper.ts
@@ -6,14 +6,8 @@ import {
   IVSCodeExtLogger,
   LogLevel,
 } from "@vscode-logging/logger";
-import {
-  listenToLogSettingsChanges,
-  logLoggerDetails,
-} from "./settings-changes-handler";
-import {
-  getLoggingLevelSetting,
-  getSourceLocationTrackingSetting,
-} from "./settings";
+import { listenToLogSettingsChanges, logLoggerDetails } from "./settings-changes-handler";
+import { getLoggingLevelSetting, getSourceLocationTrackingSetting } from "./settings";
 
 const YEOMAN_UI_LOGGER_NAME = "yeomanui";
 const YEOMAN_UI = "Application Wizard";
@@ -24,8 +18,7 @@ const WEBVIEW_RPC_LOGGER_NAME = "Webview Rpc";
  * implementation.
  */
 
-export const ERROR_LOGGER_NOT_INITIALIZED =
-  "Logger has not yet been initialized!";
+export const ERROR_LOGGER_NOT_INITIALIZED = "Logger has not yet been initialized!";
 
 /**
  * @type {IVSCodeExtLogger}
@@ -65,9 +58,7 @@ function getLibraryLogger(libraryName: string): IChildLogger {
   return getLogger().getChildLogger({ label: libraryName });
 }
 
-export function createExtensionLoggerAndSubscribeToLogSettingsChanges(
-  context: vscode.ExtensionContext
-) {
+export function createExtensionLoggerAndSubscribeToLogSettingsChanges(context: vscode.ExtensionContext) {
   createExtensionLogger(context);
   // Subscribe to Logger settings changes.
   listenToLogSettingsChanges(context);
