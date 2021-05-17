edf9145039951da38316762728526610ff78c73a
Stanislav Lvovsky <stanislav.lvovsky@sap.com>
2021-05-17 19:22:26 +0300

    chore: fix test

--- packages/frontend/test/App.spec.js
+++ packages/frontend/test/App.spec.js
@@ -16,7 +16,9 @@ describe("App.vue", () => {
 
   it("createPrompt - method", () => {
     wrapper = initComponent(App, {}, true);
-    wrapper.vm.prompts = [{ questions: [{ name: "generator", type: "list", guiType: "tiles" }] }];
+    wrapper.vm.prompts = [
+      { questions: [{ name: "generator", type: "list", guiType: "tiles" }] },
+    ];
     expect(wrapper.vm.createPrompt().name).toBe();
     expect(wrapper.vm.createPrompt([]).name).toBe();
     expect(wrapper.vm.createPrompt([], "name").name).toBe("name");
@@ -165,7 +167,9 @@ describe("App.vue", () => {
     it("no generators", () => {
       wrapper = initComponent(App, {}, true);
       wrapper.vm.promptIndex = 0;
-      wrapper.vm.prompts = [{ name: "Select Generator", questions: [{ choices: [] }] }];
+      wrapper.vm.prompts = [
+        { name: "Select Generator", questions: [{ choices: [] }] },
+      ];
       wrapper.vm.$data.messages = { select_generator_name: "Select Generator" };
       expect(wrapper.vm.isNoGenerators).toBeTruthy();
     });
@@ -186,7 +190,9 @@ describe("App.vue", () => {
     it("generators exist question.name != 'generator'", () => {
       wrapper = initComponent(App, {}, true);
       wrapper.vm.promptIndex = 0;
-      wrapper.vm.prompts = [{ name: "Select Generator", questions: [{}, { choices: [{}] }] }];
+      wrapper.vm.prompts = [
+        { name: "Select Generator", questions: [{}, { choices: [{}] }] },
+      ];
       wrapper.vm.$data.messages = { select_generator_name: "Select Generator" };
       expect(wrapper.vm.isNoGenerators).toBeTruthy();
     });
@@ -194,7 +200,9 @@ describe("App.vue", () => {
     it("prompt name != generators", () => {
       wrapper = initComponent(App, {}, true);
       wrapper.vm.promptIndex = 0;
-      wrapper.vm.prompts = [{ name: "Prompt Name", questions: [{ choices: [{}] }] }];
+      wrapper.vm.prompts = [
+        { name: "Prompt Name", questions: [{ choices: [{}] }] },
+      ];
       wrapper.vm.$data.messages = { select_generator_name: "Select Generator" };
       expect(wrapper.vm.isNoGenerators).toBeFalsy();
     });
@@ -517,14 +525,21 @@ describe("App.vue", () => {
     const event = {
       target: {
         getAttribute: jest.fn().mockImplementation((key) => {
-          return key === "command" ? "vscode.open" : key === "params" ? ["param"] : "";
+          return key === "command"
+            ? "vscode.open"
+            : key === "params"
+            ? ["param"]
+            : "";
         }),
       },
     };
     const invokeSpy = jest.spyOn(wrapper.vm.rpc, "invoke");
     wrapper.vm.executeCommand(event);
 
-    expect(invokeSpy).toHaveBeenCalledWith("executeCommand", ["vscode.open", ["param"]]);
+    expect(invokeSpy).toHaveBeenCalledWith("executeCommand", [
+      "vscode.open",
+      ["param"],
+    ]);
 
     invokeSpy.mockRestore();
   });
@@ -580,7 +595,8 @@ describe("App.vue", () => {
       wrapper.vm.stepValidated = true;
       wrapper.vm.prompts = [{}, {}];
       wrapper.vm.rpc = {
-        invoke: () => new Promise((resolve) => setTimeout(() => resolve(), 300)),
+        invoke: () =>
+          new Promise((resolve) => setTimeout(() => resolve(), 300)),
       };
 
       wrapper.vm._next();
@@ -614,7 +630,7 @@ describe("App.vue", () => {
 
       const _nextSpy = jest.spyOn(wrapper.vm, "_next");
       wrapper.vm.next();
-      await new Promise((resolve) => setTimeout(resolve, 300));
+      await new Promise((resolve) => setTimeout(() => resolve(), 300));
 
       expect(_nextSpy).toHaveBeenCalled();
       _nextSpy.mockRestore();
@@ -920,7 +936,9 @@ describe("App.vue", () => {
       wrapper.vm.promptIndex = 1;
       wrapper.vm.$data.generatorPrettyName = "testGeneratorPrettyName";
       wrapper.vm.$data.messages = { yeoman_ui_title: "yeoman_ui_title" };
-      expect(wrapper.vm.headerTitle).toEqual("yeoman_ui_title - testGeneratorPrettyName");
+      expect(wrapper.vm.headerTitle).toEqual(
+        "yeoman_ui_title - testGeneratorPrettyName"
+      );
     });
   });
 
