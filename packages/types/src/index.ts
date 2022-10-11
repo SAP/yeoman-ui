016ae9f88f93e47018d87232eb86582bb4d9560a
i057546 <iain.o-farrell@sap.com>
2022-10-11 10:57:41 +0100

    feat(app.vue): adds support for object command params
    Add support for object commadn params to allow more complex commands to be executed by Inquirer-gui
    validation links

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -59,3 +59,38 @@ export interface IPrompt {
   name: string;
   description: string;
 }
+
+/**
+ * Enhanced validation messages to support embedded help urls and commands
+ */
+ export interface IValidationLink {
+  /**
+   * Validation message to be shown to the end user
+   */
+  message: string;
+  link: {
+      /**
+       * The text associated with the help link
+       */
+      text: string;
+      /**
+       * A string base64 encoded image
+       */
+      icon?: string;
+      /**
+       * Command to be exectuted and parameters passed to the command
+       */
+      command?: {
+          id: string;
+          params: Object | string;
+      };
+      /**
+       * A http url string
+       */
+      url?: string;
+  };
+  /**
+   * Provide a stringified version of the link that will be used on the CLI
+   */
+  toString(): string;
+}
