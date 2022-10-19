59e18d3ac4aed758f5107db6ff09e924a4d116e1
Rima Sirich <rima.sirich@sap.com>
2022-10-19 18:29:37 +0300

    Revert "build: bundle minimization canceled (#717)" (#724)
    This reverts commit 7addb303ae6e5222a6088f3916b65e29bd4f2aa9.

--- packages/backend/webpack.config.js
+++ packages/backend/webpack.config.js
@@ -14,17 +14,8 @@ const config = {
     // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
     path: path.resolve(__dirname, "dist"),
     filename: "extension.js",
-    library: {
-      type: "commonjs2",
-    },
+    libraryTarget: "commonjs2",
     devtoolModuleFilenameTemplate: "../[resource-path]",
-  },
-  optimization: {
-    // The Default minimization options can sometimes cause JavaScript runtime errors.
-    // Also we don't actually need to minimize as much (not targeted for browser).
-    // Rather we mostly need to reduce the number of fileSystem access requests
-    // by reducing the number of files packaged inside our VSCode extensions
-    minimize: false,
   },
   externals: {
     vscode: "commonjs vscode",
@@ -95,8 +86,8 @@ const config = {
         test: /yeoman-environment[/|\\]lib[/|\\]util[/|\\]esm.js/,
         loader: "string-replace-loader",
         options: {
-          search: "import[(]pathToFileURL",
-          replace: "__non_webpack_require__(pathToFileURL",
+          search: "import[(]pathToFileURL[(]fileToImport",
+          replace: "__non_webpack_require__(pathToFileURL(fileToImport",
           flags: "g",
         },
       },
