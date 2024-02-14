e3850904a834c8a932e4275ac051208ad12f9d4b
Neta Molcho <35297731+NetaMolcho@users.noreply.github.com>
2024-02-14 15:20:02 +0200

    fix: generate a new project in projects folder in personal-edition plan

--- packages/backend/src/utils/constants.ts
+++ packages/backend/src/utils/constants.ts
@@ -4,6 +4,7 @@ import { homedir } from "os";
 
 class ConstantsUtil {
   public IS_IN_BAS = !isEmpty(get(process, "env.WS_BASE_URL"));
+  public IS_PERSONAL_EDITION = get(process, "env.TENANT_PLAN", "false") === "personal-edition";
   public HOMEDIR_PROJECTS: string = join(homedir(), "projects");
 }
 
