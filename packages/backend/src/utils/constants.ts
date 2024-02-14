f6c4f21e0c5faa0b5d55bffd8e44eaa7c7ffd734
Neta Molcho <35297731+NetaMolcho@users.noreply.github.com>
2024-02-14 16:35:54 +0200

    fix: adding personal-edition as part of BAS environment

--- packages/backend/src/utils/constants.ts
+++ packages/backend/src/utils/constants.ts
@@ -3,7 +3,8 @@ import { join } from "path";
 import { homedir } from "os";
 
 class ConstantsUtil {
-  public IS_IN_BAS = !isEmpty(get(process, "env.WS_BASE_URL"));
+  public IS_IN_BAS =
+    !isEmpty(get(process, "env.WS_BASE_URL")) || get(process, "env.TENANT_PLAN", "false") === "personal-edition";
   public HOMEDIR_PROJECTS: string = join(homedir(), "projects");
 }
 
