ab582962a79e962781c57a641de005e5d7e88f87
Neta Molcho <35297731+NetaMolcho@users.noreply.github.com>
2024-02-15 08:53:03 +0200

    fix: review fix

--- packages/backend/src/utils/constants.ts
+++ packages/backend/src/utils/constants.ts
@@ -4,7 +4,7 @@ import { homedir } from "os";
 
 class ConstantsUtil {
   public IS_IN_BAS =
-    !isEmpty(get(process, "env.WS_BASE_URL")) || get(process, "env.TENANT_PLAN", "false") === "personal-edition";
+    !isEmpty(get(process, "env.WS_BASE_URL")) || get(process, "env.TENANT_PLAN") === "personal-edition";
   public HOMEDIR_PROJECTS: string = join(homedir(), "projects");
 }
 
