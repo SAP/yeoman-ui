be1fec2448a171744a94761f49e323753eece052
Neta Molcho <neta.molcho@sap.com>
2024-02-18 16:01:47 +0200

    fix: checking bas mode from the bas-sdk

--- packages/backend/src/utils/constants.ts
+++ packages/backend/src/utils/constants.ts
@@ -1,10 +1,11 @@
 import { isEmpty, get } from "lodash";
 import { join } from "path";
 import { homedir } from "os";
+import * as sdk from "@sap/bas-sdk";
 
 class ConstantsUtil {
   public IS_IN_BAS =
-    !isEmpty(get(process, "env.WS_BASE_URL")) || get(process, "env.TENANT_PLAN") === "personal-edition";
+    !isEmpty(get(process, "env.WS_BASE_URL")) || sdk.devspace.getBasMode() === "personal-edition";
   public HOMEDIR_PROJECTS: string = join(homedir(), "projects");
 }
 
