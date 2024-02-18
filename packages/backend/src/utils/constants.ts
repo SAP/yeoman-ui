1aa48dbb0a2c3e72bac08d4776eeac0033a7fceb
Neta Molcho <35297731+NetaMolcho@users.noreply.github.com>
2024-02-18 16:19:43 +0200

    fix: format

--- packages/backend/src/utils/constants.ts
+++ packages/backend/src/utils/constants.ts
@@ -4,8 +4,7 @@ import { homedir } from "os";
 import * as sdk from "@sap/bas-sdk";
 
 class ConstantsUtil {
-  public IS_IN_BAS =
-    !isEmpty(get(process, "env.WS_BASE_URL")) || sdk.devspace.getBasMode() === "personal-edition";
+  public IS_IN_BAS = !isEmpty(get(process, "env.WS_BASE_URL")) || sdk.devspace.getBasMode() === "personal-edition";
   public HOMEDIR_PROJECTS: string = join(homedir(), "projects");
 }
 
