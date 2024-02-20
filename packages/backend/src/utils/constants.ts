1a5bdb3facc10ea7137c21a84677d11b61e3e097
Neta Molcho <35297731+NetaMolcho@users.noreply.github.com>
2024-02-20 10:52:14 +0200

    fix: adding personal-edition as part of BAS environment

--- packages/backend/src/utils/constants.ts
+++ packages/backend/src/utils/constants.ts
@@ -1,9 +1,10 @@
 import { isEmpty, get } from "lodash";
 import { join } from "path";
 import { homedir } from "os";
+import { devspace } from "@sap/bas-sdk";
 
 class ConstantsUtil {
-  public IS_IN_BAS = !isEmpty(get(process, "env.WS_BASE_URL"));
+  public IS_IN_BAS = !isEmpty(get(process, "env.WS_BASE_URL")) || devspace.getBasMode() === "personal-edition";
   public HOMEDIR_PROJECTS: string = join(homedir(), "projects");
 }
 
