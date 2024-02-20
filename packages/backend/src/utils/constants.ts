7f5abe6ddf0af7c0031bc8e37e9af4b2dce8e17e
Neta Molcho <35297731+NetaMolcho@users.noreply.github.com>
2024-02-20 10:30:35 +0200

    Update constants.ts
    Co-authored-by: AG <33512452+alex-gilin@users.noreply.github.com>

--- packages/backend/src/utils/constants.ts
+++ packages/backend/src/utils/constants.ts
@@ -4,7 +4,7 @@ import { homedir } from "os";
 import { devspace } from "@sap/bas-sdk";
 
 class ConstantsUtil {
-  public IS_IN_BAS = !isEmpty(get(process, "env.WS_BASE_URL")) || sdk.devspace.getBasMode() === "personal-edition";
+  public IS_IN_BAS = !isEmpty(get(process, "env.WS_BASE_URL")) || devspace.getBasMode() === "personal-edition";
   public HOMEDIR_PROJECTS: string = join(homedir(), "projects");
 }
 
