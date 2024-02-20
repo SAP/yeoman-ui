58115b8e0e5ad44a41b297816935f232861fa3b5
Neta Molcho <35297731+NetaMolcho@users.noreply.github.com>
2024-02-20 10:48:35 +0200

    fix: format

--- packages/backend/src/utils/constants.ts
+++ packages/backend/src/utils/constants.ts
@@ -1,7 +1,7 @@
 import { isEmpty, get } from "lodash";
 import { join } from "path";
 import { homedir } from "os";
-import { devspace } from "@sap/bas-sdk"; 
+import { devspace } from "@sap/bas-sdk";
 
 class ConstantsUtil {
   public IS_IN_BAS = !isEmpty(get(process, "env.WS_BASE_URL")) || devspace.getBasMode() === "personal-edition";
