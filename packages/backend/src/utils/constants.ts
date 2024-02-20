750922de5596ba628f8b995be19a3425e1061637
Neta Molcho <35297731+NetaMolcho@users.noreply.github.com>
2024-02-20 10:30:20 +0200

    Update constants.ts
    Co-authored-by: AG <33512452+alex-gilin@users.noreply.github.com>

--- packages/backend/src/utils/constants.ts
+++ packages/backend/src/utils/constants.ts
@@ -1,7 +1,7 @@
 import { isEmpty, get } from "lodash";
 import { join } from "path";
 import { homedir } from "os";
-import * as sdk from "@sap/bas-sdk";
+import { devspace } from "@sap/bas-sdk";
 
 class ConstantsUtil {
   public IS_IN_BAS = !isEmpty(get(process, "env.WS_BASE_URL")) || sdk.devspace.getBasMode() === "personal-edition";
