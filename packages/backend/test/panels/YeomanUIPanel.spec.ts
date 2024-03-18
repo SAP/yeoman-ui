db8a0ec84af0fc1769213c2030e29c6633fe2064
Vered Constantin1 <vered.constantin@sap.com>
2024-03-18 12:41:47 +0200

    fix: refactor test

--- packages/backend/test/panels/YeomanUIPanel.spec.ts
+++ packages/backend/test/panels/YeomanUIPanel.spec.ts
@@ -275,32 +275,37 @@ describe("YeomanUIPanel unit test", () => {
   });
 
   describe("dispose", () => {
-    it("dispose - calling usage analytics when panel is manually closed.", () => {
-      const objYeomanui: any = {
-        generatorName: "generator-name",
-        promptCount: 1,
-        gen: {
-          prompts: {
-            items: [
-              {
-                name: "step1",
-              },
-              {
-                name: "step2",
-              },
-            ],
-          },
+    const objYeomanui: any = {
+      generatorName: "generator-name",
+      promptCount: 1,
+      gen: {
+        prompts: {
+          items: [
+            {
+              name: "step1",
+            },
+            {
+              name: "step2",
+            },
+          ],
         },
-      };
+      },
+    };
+
+    const webviewPanel = {
+      dispose: () => {},
+    };
+
+    beforeEach(() => {
       set(panel, "yeomanui", objYeomanui);
-      const webviewPanel = {
-        dispose: () => {},
-      };
       set(panel, "webViewPanel", webviewPanel);
       set(panel, "disposables", []);
       set(panel, "cleanFlowPromise", () => {});
 
       commandsMock.expects("executeCommand").withExactArgs("setContext", "yeomanUI.Focused", false).resolves();
+    });
+
+    it("dispose - calling usage analytics when panel is manually closed.", () => {
       trackerWrapperMock
         .expects("updateGeneratorClosedManually")
         .withArgs("generator-name", "step1", 1, 2)
@@ -310,36 +315,8 @@ describe("YeomanUIPanel unit test", () => {
     });
 
     it("dispose - not calling usage analytics when generation ended and user clicked finish.", () => {
-      const objYeomanui: any = {
-        generatorName: "generator-name",
-        promptCount: 1,
-        gen: {
-          prompts: {
-            items: [
-              {
-                name: "step1",
-              },
-              {
-                name: "step2",
-              },
-            ],
-          },
-        },
-      };
-      set(panel, "yeomanui", objYeomanui);
-      const webviewPanel = {
-        dispose: () => {},
-      };
       set(webviewPanel, Constants.GENERATOR_COMPLETED, true);
-      set(panel, "webViewPanel", webviewPanel);
-      set(panel, "disposables", []);
-      set(panel, "cleanFlowPromise", () => {});
-
-      commandsMock.expects("executeCommand").withExactArgs("setContext", "yeomanUI.Focused", false).resolves();
-      trackerWrapperMock
-        .expects("updateGeneratorClosedManually")
-        .never()
-        .resolves();
+      trackerWrapperMock.expects("updateGeneratorClosedManually").never().resolves();
       panel["dispose"]();
     });
   });
